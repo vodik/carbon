723fb0f5b00f3c3cd2bc5b94712468c5c51754c5
Simon Gomizelj <simongmzlj@gmail.com>
2012-08-17 18:44:46 -0400

    replace perror with err

--- tty.c
+++ tty.c
@@ -4,6 +4,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
+#include <errno.h>
+#include <err.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/ioctl.h>
@@ -24,39 +26,30 @@ tty_t *tty_new(shellfn shell)
     if ((masterfd = posix_openpt(O_RDWR | O_NOCTTY)) == -1
         || grantpt(masterfd) == -1
         || unlockpt(masterfd) == -1
-        || (slavedev = ptsname(masterfd)) == NULL) {
-        perror("failed to create pts\n");
-        exit(EXIT_FAILURE);
-    }
+        || (slavedev = ptsname(masterfd)) == NULL)
+        err(EXIT_FAILURE, "failed to create pts\n");
 
     slavefd = open(slavedev, O_RDWR | O_NOCTTY);
-    if (slavefd == -1) {
-        perror("open");
-        exit(EXIT_FAILURE);
-    }
+    if (slavefd == -1)
+        err(EXIT_FAILURE, "open");
 
     tty_t *tty = malloc(sizeof(tty_t));
-    if (!tty) {
-        perror("malloc");
-        exit(EXIT_FAILURE);
-    }
+    if (!tty)
+        err(EXIT_FAILURE, "malloc");
 
     switch (pid = fork()) {
     case -1:
-        perror("fork");
-        exit(EXIT_FAILURE);
+        err(EXIT_FAILURE, "fork");
         break;
     case 0:
         setsid();
         dup2(slavefd, STDIN_FILENO);
         dup2(slavefd, STDOUT_FILENO);
         dup2(slavefd, STDERR_FILENO);
-        if (ioctl(slavefd, TIOCSCTTY, NULL) == -1) {
-            perror("ioctl");
-            exit(EXIT_FAILURE);
-        }
+        if (ioctl(slavefd, TIOCSCTTY, NULL) == -1)
+            err(EXIT_FAILURE, "ioctl");
         shell();
-        exit(-1); /* shouldn't get here */
+        errx(EXIT_FAILURE, "shellfn didn't exit");
         break;
     }
 
@@ -71,10 +64,8 @@ void tty_poll(tty_t *tty, int epfd, int op)
     tty->event.events = EPOLLIN | EPOLLET;
     tty->event.data.ptr = tty;
 
-    if (epoll_ctl(epfd, op, tty->fd, &tty->event) == -1) {
-        perror("epoll_ctl");
-        exit(EXIT_FAILURE);
-    }
+    if (epoll_ctl(epfd, op, tty->fd, &tty->event) == -1)
+        err(EXIT_FAILURE, "epoll_ctl");
 }
 
 void tty_read(tty_t *t)
@@ -84,15 +75,11 @@ void tty_read(tty_t *t)
 
     ret = read(t->fd, buf, BUFSIZ);
     printf("read %d\n", ret);
-    if (ret == -1) {
-        perror("read");
-        exit(EXIT_FAILURE);
-    }
+    if (ret == -1)
+        err(EXIT_FAILURE, "read");
 
     /* tty_put(t, buf, ret); */
     ret = write(STDOUT_FILENO, buf, ret);
-    if (ret == -1) {
-        perror("write");
-        exit(EXIT_FAILURE);
-    }
+    if (ret == -1)
+        err(EXIT_FAILURE, "write");
 }
